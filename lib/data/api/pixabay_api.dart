052dea3312a05d9f971f875a409cb1b60a083645
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-05 00:06:27 +0900

    feat: pixabay api 구현

---
+++ lib/data/api/pixabay_api.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:image_search_3day/data/dto/pixabay_result_dto.dart';
+
+class PixabayApi {
+  Future<PixabayResultDto> getImages(String query) async {
+    final url =
+        'https://pixabay.com/api/?key=37971457-4bad1cc05aa70353cb5a9d52e&q=$query&image_type=photo';
+    final http.Response response = await http.get(Uri.parse(url));
+    Map<String, dynamic> jsonString = jsonDecode(response.body);
+    return PixabayResultDto.fromJson(jsonString);
+  }
+}
