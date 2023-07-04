448e4fd1275199e29de2b902af98710c1b7bf761
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-05 00:45:41 +0900

    fix: changeNotifier 설정, detail page extra 추가

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_search_3day/data/model/photo.dart';
+import 'package:image_search_3day/data/repository/pixabay_photo_repository_impl.dart';
 import 'package:image_search_3day/ui/detail/detail_screen.dart';
 import 'package:image_search_3day/ui/main/main_screen.dart';
+import 'package:image_search_3day/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const MainScreen();
+        return ChangeNotifierProvider(
+          create: (_) => MainViewModel(PixabayPhotoRespositoryImpl()),
+          child: const MainScreen(),
+        );
       },
       routes: <RouteBase>[
         GoRoute(
           path: 'detail',
           builder: (BuildContext context, GoRouterState state) {
-            return const DetailScreen();
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
           },
         ),
       ],
