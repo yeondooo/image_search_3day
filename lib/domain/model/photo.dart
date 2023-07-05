2cd661cb2f28cdc21e73dbf7a31cff4fba6c5b43
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-05 21:11:26 +0900

    fix: model class freezed 사용하여 수정

--- lib/domain/model/photo.dart
+++ lib/domain/model/photo.dart
@@ -1,22 +1,18 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'photo.freezed.dart';
 
 part 'photo.g.dart';
 
-@JsonSerializable(explicitToJson: true)
-class Photo {
-  final num id;
-  final String webformatURL;
-  final num views;
-  final num likes;
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required num id,
+    required String webformatURL,
+    required num views,
+    required num likes,
+  }) = _Photo;
 
-  Photo({
-    required this.id,
-    required this.webformatURL,
-    required this.views,
-    required this.likes,
-  });
-
-  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
-
-  Map<String, dynamic> toJson() => _$PhotoToJson(this);
+  factory Photo.fromJson(Map<String, Object?> json) => _$PhotoFromJson(json);
 }
